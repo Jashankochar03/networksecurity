98de7eaf1f44d64d1440e5be0997223c47597ae4
Jashanpreet Singh <jaskochar2003@gmail.com>
2025-07-20 23:18:16 +0530

    DagsHub added

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 import sys
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
@@ -23,6 +24,13 @@ if __name__=="__main__":
         data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
         data_transformation = DataTransformation(data_validation_artifact,data_transformation_config)
         data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("data transformation completed")
+        model_trainer_config = ModelTrainerConfig(trainingpipelineconfig)
+
+        logging.info("model training started")
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info("model training completed")
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -12,6 +12,11 @@ ARTIFACT_DIR:str = "Artifacts"
 FILE_NAME:str = "phisingData.csv"
 TRAIN_FILE_NAME:str = "train.csv"
 TEST_FILE_NAME:str = "test.csv"
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+
+SAVED_MODEL_DIR =os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
+
 
 SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
 
